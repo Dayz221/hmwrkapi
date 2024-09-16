731bdeb029a05debc6be28d1bf3ddaad77e709e6
Dayz221 <rutuchenal37@gmail.com>
2024-09-16 22:27:29 +0300

    s

--- index.js
+++ index.js
@@ -11,27 +11,12 @@ import { TELEGRAM_BOT_PASSWORD } from "./config.js"
 import Group from "./models/group.js"
 import apiRouter from "./routes/apiRouter.js"
 import cors from "cors"
+import https from "https"
+import fs from "fs"
+import path from "path"
 // import teleBot from "./teleBot.js"
 
-const PORT = process.env.PORT || 80
-
-const app = express()
-
-app.use(express.json())
-app.use(logger)
-app.use(cors())
-app.use(express.static('static'))
-app.use("/api/auth", authRouter)
-app.use("/api/tasks", taskRouter)
-app.use("/api/files", fileRouter)
-app.use("/api", apiRouter)
-
-app.get('/', (req, res) => res.status(200).send({ message: "homework site and telegram-bot api" }))
-
-app.listen(PORT, async (err) => {
-    if (err) return console.log(color.red(err))
-
-    await mongoose
+mongoose
         .connect("mongodb+srv://admin:qwedcvhu123@cluster0.7iifv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
         .then(async () => { 
             console.log(color.green(`MongoDB attached!`))
@@ -53,7 +38,33 @@ app.listen(PORT, async (err) => {
         })
         .catch((err) => { console.log(color.red(err)) })
 
+const app = express()
+
+app.use(express.json())
+app.use(logger)
+app.use(cors())
+app.use(express.static('static'))
+app.use("/api/auth", authRouter)
+app.use("/api/tasks", taskRouter)
+app.use("/api/files", fileRouter)
+app.use("/api", apiRouter)
+
+app.get('/', (req, res) => res.status(200).send({ message: "homework site and telegram-bot api" }))
+
+app.listen(8080, (err) => {
+    if (err) return console.log(color.red(err))
     console.log(color.green(`Server started on http://localhost:${PORT}`))
 })    
+
+https.createServer(
+    {
+        cert: fs.readFileSync('./cert/fullchain.pem'),
+        key: fs.readFileSync('./cert/privkey.pem')
+    },
+    app
+).listen(8443, (err) => {
+    if (err) return console.log(color.red(err))
+    console.log(color.green(`Server started on http://localhost:${PORT}`))
+})
 
 export default app
